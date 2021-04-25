105df3625f4cfcbf5cea8ee38198a2c7ef46a2b0
Tgsouza <72031342+Tgsouza@users.noreply.github.com>
2021-04-25 07:56:18 +0100

    Password Validator
    Pass Validator ready. Started to format the password validator box and the navBar.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,26 +1,107 @@
+//PASSWORD CHECK//
 function verifyPass(){
-    alert("works!");
-    
-    //Getting input from the user
+     
+//Getting input from the user
     var pass = document.getElementById("pswd").value;
 
-    /*Variable with the RegEx containing the rules to a valid password    
-    (?=.*[a-z]) - Must contain at least one lowercase char
-    (?=.*[A-Z]) - Must contain at least one uppercase char
-    (?=.*[0-9]) - Must contain one number from
-    (?=.*[!@#$%^&*]) - Must contain one special char
-    (?=.{8,}) - Must have at least 8 char*/
-    var passRules = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"); 
-
-    //VALIDATION//
+/*Variable with the RegEx containing the rules to a valid password    
+(?=.*[a-z]) - Must contain at least one lowercase char
+(?=.*[A-Z]) - Must contain at least one uppercase char
+(?=.*[0-9]) - Must contain one number from
+(?=.*[!@#$%^&*]) - Must contain one special char
+(?=.{8,}) - Must have at least 8 char*/
+    var passRules = /"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"/;
+    
+//PASS VALIDATION//
     if(pass ==""){
+    
         document.getElementById("message").innerHTML="Fill the password to proceed!";
         return false;
 
-    }
-    if(pass != passRules){
-        document.getElementById("message").innerHTML="The password must contain at least 8 digits!";
+    }else if (pass.match(/[a-z]/g) && pass.match(/[A-Z]/g) && pass.match(/[!@#$%^&*]/g)&& pass.match(/[0-9]/g) && pass.match(/[^a-zA-Z\d]/g) && pass.length >= 8){
+
+        document.getElementById("message").innerHTML="Valid Password";
+        document.getElementById("message").style.color = "Green";
         return false;
+
+    }else{
+
+        document.getElementById("message").innerHTML="Invalid ! Check the password validations!";
+        document.getElementById("message").style.color = "Red";
+        return false;
+
     }
 
+}
+
+//STRONG VALIDATION BOX//
+var myInput = document.getElementById("pswd");
+var lowerCase = document.getElementById("lowerCase");
+var upperCase = document.getElementById("upperCase");
+var specChar = document.getElementById("specChar");
+var hasNum = document.getElementById("hasNum");
+var minChar = document.getElementById("minChar");
+
+//Show Validation box//
+myInput.onfocus = function(){
+
+    document.getElementById("passValidation").style.display="block";
+
+}
+
+//Hide the validation box//
+myInput.onblur = function(){
+
+    document.getElementById("passValidation").style.display="none";
+
+}
+
+//Check the password strength when user starts to type//
+
+myInput.onkeyup = function(){
+
+    var lowerCheck = /[a-z]/g;
+    if(myInput.value.match(lowerCheck)){
+        lowerCase.classList.remove("invalid");
+        lowerCase.classList.add("valid");
+
+    }else{
+        lowerCase.classList.remove("valid");
+        lowerCase.classList.add("invalid");
+    }
+
+    var upperCheck = /[A-Z]/g;
+    if(myInput.value.match(upperCheck)){
+        upperCase.classList.remove("invalid");
+        upperCase.classList.add("valid");
+    }else{
+        upperCase.classList.remove("valid");
+        upperCase.classList.add("invalid");
+    }
+
+    var numCheck = /[0-9]/g;
+    if(myInput.value.match(numCheck)){
+        hasNum.classList.remove("invalid");
+        hasNum.classList.add("valid");
+    }else{
+        hasNum.classList.remove("valid");
+        hasNum.classList.add("invalid");
+    }
+
+    var specCheck = /[!@#$%^&*]/g;
+    if(myInput.value.match(specCheck)){
+        specChar.classList.remove("invalid");
+        specChar.classList.add("valid");
+    }else{
+        specChar.classList.remove("valid");
+        specChar.classList.add("invalid");
+    }
+
+    if(myInput.value.length >= 8){
+        minChar.classList.remove("invalid");
+        minChar.classList.add("valid");
+    }else{
+        minChar.classList.remove("valid");
+        minChar.classList.add("invalid");
+    }
 }
